3edc30e90bf326f306d2f972fb590a77e38d86fa
Shahadat-Murshed <shahadat.murshed@gmail.com>
2024-03-26 18:43:56 +0600

    Template Mastering Initiate

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,6 +21,8 @@ class UserSeeder extends Seeder
                 'email' => 'super@gmail.com',
                 'role' => 'admin',
                 'status' => 1,
+                'is_super' => 1,
+                'email_verified_at' => now(),
                 'password' => bcrypt('super'),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
@@ -32,6 +34,8 @@ class UserSeeder extends Seeder
                 'email' => 'admin@gmail.com',
                 'role' => 'admin',
                 'status' => 1,
+                'is_super' => 0,
+                'email_verified_at' => now(),
                 'password' => bcrypt('admin'),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
@@ -43,11 +47,14 @@ class UserSeeder extends Seeder
                 'email' => 'user@gmail.com',
                 'role' => 'user',
                 'status' => 1,
+                'is_super' => 0,
+                'email_verified_at' => now(),
                 'password' => bcrypt('user'),
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
         ]);
+        \App\Models\User::factory(10)->create();
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+Route::get('master', function () {
+    return view('admin.layouts.master');
+})->name('master');
